929dcc5da70ee4eb64ae37f3cfc5bac10bda1dc2
Emerson Invillia <157976266+emersoninvillia@users.noreply.github.com>
2024-02-26 23:32:25 +0000

    8th Commit

--- server.js
+++ server.js
@@ -3,33 +3,40 @@ const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const path = require('path');
 
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
-// Set some defaults if your JSON file is empty
 db.defaults({ users: [] }).write();
 
 const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/login.html'));
+});
+
 app.post('/signup', async (req, res) => {
   const { username, password } = req.body;
-
-  // Check if username already exists
   const userExists = db.get('users').find({ username }).value();
   if (userExists) {
     return res.status(400).send('Username already exists');
   }
-
-  // Hash the password
   const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-  // Store the user in the database
   db.get('users').push({ username, password: hashedPassword }).write();
-
   res.status(200).send('User created');
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = db.get('users').find({ username }).value();
+  if (user && await bcrypt.compare(password, user.password)) {
+    res.status(200).send('Logged in');
+  } else {
+    res.status(401).send('Invalid username or password');
+  }
 });
 
 app.listen(3000, () => console.log('Server started on port 3000'));
